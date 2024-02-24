cba8b0a3c6f62942217501be605746b2dc57cbef
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2024-02-23 19:15:35 -0800

    Initial Commit & Tailwind Init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className="h-screen bg-white dark:bg-[#262B3C]">
+      <nav>Wordle</nav>
+      <section>Words</section>
+      <section>Keyboard</section>
+    </main>
+  )
+}
+
+export default App
